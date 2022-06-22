e8a38a09f22ed74ddcdbf3aa77aeb71688cfd311
Samuel Benitez <samuelebenitez21@gmail.com>
2022-06-22 16:15:55 -0300

    _document.js added to try fonts

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* link your fonts here */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Jost:wght@100;300;400&family=Krona+One&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
